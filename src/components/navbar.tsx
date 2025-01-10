617f777a0a4731981a6dbf6da70a77d8a2227c16
niadulMunna <niadulislammunna@gmail.com>
2025-01-10 23:32:57 +0600

    navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { NavTab } from "@/motion-primitives/navTab";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      <NavTab />
+    </div>
+  );
+};
+
+export default Navbar;
