7c7e949dc5677c3a4ed812fcd6fccac95dbef9d1
niadulMunna <niadulislammunna@gmail.com>
2025-01-11 22:25:36 +0600

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,41 @@
-import { NavTab } from "@/motion-primitives/navTab";
+import Link from "next/link";
 import React from "react";
+
+const menu = [
+  {
+    title: "Home",
+    href: "#",
+  },
+  {
+    title: "Products",
+    href: "#",
+  },
+  {
+    title: "Components",
+    href: "#",
+  },
+  {
+    title: "Pricing",
+    href: "#",
+  },
+  {
+    title: "Blog",
+    href: "#",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div className="">
-      <NavTab />
+    <div className="flex items-center gap-4 p-5 bg-neutral-300 text-center  justify-center">
+      {menu.map((item, idx) => (
+        <Link
+          className="hover:text-[18px] duration-300 hover: px-4 py-2 rounded "
+          key={idx}
+          href={item.href}
+        >
+          {item.title}
+        </Link>
+      ))}
     </div>
   );
 };
