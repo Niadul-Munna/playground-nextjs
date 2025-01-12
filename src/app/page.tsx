76f1384ddeee1888ae58d16f33ee7d0f68585976
niadulMunna <niadulislammunna@gmail.com>
2025-01-12 21:23:50 +0600

    added herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Docks } from "@/components/docks";
+import HeroSection from "@/components/heroSection";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
       <Docks />
     </div>
   );
