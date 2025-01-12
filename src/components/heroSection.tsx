4d313a60d52384b1867de0140f6aab3c893b1f43
niadulMunna <niadulislammunna@gmail.com>
2025-01-12 21:23:56 +0600

    hero

---
+++ src/components/heroSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <section className="bg-[#0A2540] h-screen flex justify-center items-center">
+            <div className="text-center">
+                <h1 className="text-6xl text-white font-bold">Welcome to my playground</h1>
+                <p className="text-2xl text-white mt-4">A place where i experiment with new technologies and ideas</p>
+            </div>
+        </section>
+        
+    </div>
+  )
+}
+
+export default HeroSection
