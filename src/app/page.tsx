87b533798759d3805ad82accf56ff8ab541a87fb
niadulMunna <niadulislammunna@gmail.com>
2025-01-12 21:15:22 +0600

    motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Docks } from "@/components/docks";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Docks />
     </div>
   );
 }
