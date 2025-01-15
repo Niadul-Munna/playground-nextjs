4cc73a083991c806e42312d74164c4d10ecb3c08
niadulMunna <niadulislammunna@gmail.com>
2025-01-15 23:59:41 +0600

    comp page

---
+++ src/app/components/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Components = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">Components</h1>
+      <p className="mt-4 text-2xl">
+        This page is a collection of components that can be used in the app
+      </p>
+    </div>
+  );
+};
+
+export default Components;
