04ed8c4790f4c89180c274ff450281ed174a22bb
niadulMunna <niadulislammunna@gmail.com>
2025-01-17 23:04:37 +0600

    blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,38 @@
+/*************  ✨ Codeium Command ⭐  *************/
+"use client";
+
+import { BlogPost } from "@/components/blog-post";
+
+const Blog = () => {
+  return (
+    <div className="max-w-2xl mx-auto py-16 px-4">
+      <h1 className="text-4xl font-bold mb-8">Blog</h1>
+      <BlogPost
+        title="My first blog post"
+        date="2022-07-01"
+        content={
+          <p>
+            This is my first blog post. I'm so excited to finally have a blog.
+            I've been wanting to start one for a while now, but I just couldn't
+            seem to get started. But now that I have, I'm excited to share my
+            thoughts and experiences with all of you.
+          </p>
+        }
+      />
+      <BlogPost
+        title="My second blog post"
+        date="2022-07-02"
+        content={
+          <p>
+            This is my second blog post. I'm still getting the hang of this
+            whole blogging thing, but I'm enjoying it so far. I hope you're
+            enjoying it too.
+          </p>
+        }
+      />
+    </div>
+  );
+};
+
+export default Blog;
+/******  4c339f55-c05c-4375-9e63-072608b8a58c  *******/
