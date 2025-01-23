ee3dbab980c03e0788cedebe84af713ea4084eef
niadulMunna <niadulislammunna@gmail.com>
2025-01-23 22:20:02 +0600

    footer added

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+/*************  ✨ Codeium Command ⭐  *************/
+export function Footer() {
+  return (
+    <footer className="py-12 bg-primary-foreground text-primary">
+      <div className="container mx-auto px-4">
+        <p className="text-center">
+          Copyright &copy; {new Date().getFullYear()} All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+/******  4759ac46-a18e-41fa-8d01-8bbfd8437e3b  *******/
