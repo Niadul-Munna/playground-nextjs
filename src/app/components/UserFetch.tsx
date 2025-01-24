d59b7c40f06f6276854d56176e88e31e22d27c3f
niadulMunna <niadulislammunna@gmail.com>
2025-01-24 21:48:28 +0600

    refactor

---
+++ src/app/components/UserFetch.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React, { useState } from "react";
+
+const Components = () => {
+  const [users, setUsers] = useState([]);
+
+  fetch("https://jsonplaceholder.typicode.com/todos/user")
+    .then((response) => response.json())
+    .then(setUsers);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {users.map((user, inx) => (
+        <div key={inx}>{user}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Components;
