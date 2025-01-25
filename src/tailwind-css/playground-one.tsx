2947b4976ee02a30967a47fa147c2ea117a2e831
niadulMunna <niadulislammunna@gmail.com>
2025-01-26 00:42:06 +0600

    style

--- src/tailwind-css/playground-one.tsx
+++ src/tailwind-css/playground-one.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PlaygroundOne = () => {
-  return <div>PlaygroundOne</div>;
+  return (
+    <div className="bg-red-500 w-screen h-screen container mx-auto">
+      PlaygroundOne
+    </div>
+  );
 };
 
 export default PlaygroundOne;
