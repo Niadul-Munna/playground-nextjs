26b9b24652228a6abd50711bdf9e8f3e99339477
niadulMunna <niadulislammunna@gmail.com>
2025-01-31 04:08:32 +0600

    refactor

--- src/tailwind-css/playground-one.tsx
+++ src/tailwind-css/playground-one.tsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const PlaygroundOne = () => {
   return (
-    <div className="bg-red-500 w-screen h-screen container mx-auto">
-      PlaygroundOne
+    <div className="bg-lime-200 w-screen h-screen container mx-auto">
+      <div className="columns-3 ...">
+        <Image
+          className="aspect-3/2 ..."
+          src=""
+          alt=""
+          width={200}
+          height={200}
+        />
+        <Image
+          className="aspect-3/2 ..."
+          src=""
+          alt=""
+          width={200}
+          height={200}
+        />
+        <Image
+          className="aspect-3/2 ..."
+          src=""
+          alt=""
+          width={200}
+          height={200}
+        />
+      </div>
     </div>
   );
 };
