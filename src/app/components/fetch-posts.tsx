f09b69d30a1030982db89431661b6bf4dcee09e0
niadulMunna <niadulislammunna@gmail.com>
2025-01-31 23:19:23 +0600

    refactor

--- src/app/components/fetch-posts.tsx
+++ src/app/components/fetch-posts.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { getPost } from "@/api/get-post";
+import { deletePost, getPost } from "@/api/get-post";
+import { Button } from "@/components/ui/button";
 import React, { useEffect, useState } from "react";
 
 interface Post {
@@ -7,26 +8,62 @@ interface Post {
   title: string;
   body: string;
 }
+
 const FetchPosts = () => {
   const [postData, setPostData] = useState<Post[]>([]);
 
+  const handleDeletePost = async (id: number) => {
+    try {
+      const res = await deletePost(id);
+      if (res.status === 200) {
+        const newUpdatedPosts = postData.filter((item) => {
+          return item.id !== id;
+        });
+        setPostData(newUpdatedPosts);
+      } else {
+        console.log("Failed to delete the post", res.status);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const getpostData = async () => {
-      const res = await getPost();
-      setPostData(res.data);
-    };
-    getpostData();
+    try {
+      const getpostData = async () => {
+        const res = await getPost();
+        setPostData(res.data);
+      };
+      getpostData();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
   return (
     <div className="bg-gray-300">
-      <ul className="grid lg:grid-cols-3 md:grid-cols-2  grid-cols-1 gap-5 ">
+      <ol className="grid lg:grid-cols-3 md:grid-cols-2  grid-cols-1 gap-5 list-decimal list-inside ">
         {postData.map((post) => (
-          <li className="bg-blue-300  p-4 space-y-2" key={post.id}>
+          <li
+            className="bg-blue-300  p-5
+           space-y-2 rounded-2xl"
+            key={post.id}
+          >
             <h1 className="text-xl">Title: {post.title}</h1>
             <p>Body: {post.body}</p>
+            <div className="space-x-2">
+              <Button className="bg-green-500 hover:bg-green-600 hover:shadow-2xl">
+                Edit
+              </Button>
+              <Button
+                onClick={() => handleDeletePost(post.id)}
+                variant="destructive"
+              >
+                Delete
+              </Button>
+            </div>
           </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 };
