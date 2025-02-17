0fe36fa3e14cb7e28f45a2af438d2f690f9d18bd
niadulMunna <niadulislammunna@gmail.com>
2025-02-17 23:49:18 +0600

    add filter item

---
+++ src/app/components/filter-items.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import { data } from "@/app/data/data";
+const FilterItems = () => {
+  console.log(data);
+  return <div></div>;
+};
+
+export default FilterItems;
