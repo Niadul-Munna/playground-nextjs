55f1ffa656f287f46daea6ce430f69b5ccbdcaa0
niadulMunna <niadulislammunna@gmail.com>
2025-02-17 23:49:03 +0600

    added data table

---
+++ src/app/components/data-table.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { data } from "@/app/data/data";
+const DataTable = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <table className="text-center">
+        <thead className="bg-blue-300 uppercase text-lg">
+          <tr>
+            <th>id</th>
+            <th>first</th>
+            <th>last name</th>
+            <th>email</th>
+            <th>gender</th>
+            <th>phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, idx) => (
+            <tr className="odd:bg-neutral-300 border border-black" key={idx}>
+              <td>{item.id}</td>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
+              <td>{item.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
