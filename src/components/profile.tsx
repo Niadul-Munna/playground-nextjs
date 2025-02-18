c7d342c78392ad036ef6824619382874d6594666
niadulMunna <niadulislammunna@gmail.com>
2025-02-19 00:32:35 +0600

    feat: profile comp

---
+++ src/components/profile.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useContext } from "react";
+import UserContext from "@/context/user-context";
+
+const Profile = () => {
+  const { user } = useContext<any>(UserContext);
+  if (!user) return <div className="bg-red-500 p-1">Please Login</div>;
+  else
+    return (
+      <div className="bg-green-400 p-1">
+        Welcome <span className="uppercase font-bold">{user.username},</span>
+      </div>
+    );
+};
+
+export default Profile;
