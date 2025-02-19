96d23c85c6cb4defb5591c3f4a66307c041071f8
niadulMunna <niadulislammunna@gmail.com>
2025-02-20 00:29:42 +0600

    added login and profile comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import HeroSection from "@/components/heroSection";
 // import LikeButton from "./components/likeButton";
 // import FetchPosts from "./components/fetch-posts";
 import DataTable from "./components/data-table";
+import Login from "@/components/login";
+import Profile from "@/components/profile";
 
 export default function Home() {
   return (
     <div>
-      <HeroSection />
+      {/* <HeroSection /> */}
       {/* <LikeButton /> */}
-      <DataTable />
+      {/* <DataTable /> */}
       {/* <FetchPosts /> */}
+      <Login />
+      <Profile />
     </div>
   );
 }
