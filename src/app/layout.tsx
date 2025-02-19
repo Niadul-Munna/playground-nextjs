40dbc69502764ed0eea40826b0dd6ffc3934a4bd
niadulMunna <niadulislammunna@gmail.com>
2025-02-20 00:30:04 +0600

    wrap with UserContextProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import UserContextProvider from "@/context/user-context-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
-          <Navbar />
-        </nav>
-        {children}
-        <Footer />
+        <UserContextProvider>
+          <nav>
+            <Navbar />
+          </nav>
+          {children}
+          <Footer />
+        </UserContextProvider>
       </body>
     </html>
   );
