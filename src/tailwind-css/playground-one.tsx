56eb74dc1a4cbb37079cda95acbca53cc6330a7e
nidul-munna <niadulislammunna@gmail.com>
2025-02-20 17:47:54 +0000

    style

--- src/tailwind-css/playground-one.tsx
+++ src/tailwind-css/playground-one.tsx
@@ -4,23 +4,23 @@ import React from "react";
 const PlaygroundOne = () => {
   return (
     <div className="bg-lime-200 w-screen h-screen container mx-auto">
-      <div className="columns-3 ...">
+      <div className="columns-3">
         <Image
-          className="aspect-3/2 ..."
+          className="aspect-3/2"
           src=""
           alt=""
           width={200}
           height={200}
         />
         <Image
-          className="aspect-3/2 ..."
+          className="aspect-3/2"
           src=""
           alt=""
           width={200}
           height={200}
         />
         <Image
-          className="aspect-3/2 ..."
+          className="aspect-3/2"
           src=""
           alt=""
           width={200}
