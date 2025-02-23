e2b559e85423e7a4faaa0333a35839bf24f74b06
niadulMunna <niadulislammunna@gmail.com>
2025-02-23 21:44:47 +0600

    style: changes some css

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -7,7 +7,7 @@ const Profile = () => {
  if (!user) return <div className="bg-red-300 p-1">Please Login</div>;
   else
     return (
-      <div className="bg-green-400 p-1">
+      <div className="bg-green-400 px-3 py-1.5">
         Welcome <span className="uppercase font-bold">{user.username},</span>
       </div>
     );
