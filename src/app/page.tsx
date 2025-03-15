354190f38d52907fd08ce5371cbb911caf4c034e
niadulMunna <niadulislammunna@gmail.com>
2025-03-15 22:12:30 +0600

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Docks } from "@/components/docks";
 import HeroSection from "@/components/heroSection";
-// import LikeButton from "./components/likeButton";
-// import FetchPosts from "./components/fetch-posts";
+import LikeButton from "./components/likeButton";
+import FetchPosts from "./components/fetch-posts";
 import DataTable from "./components/data-table";
 import Login from "@/components/login";
 import Profile from "@/components/profile";
@@ -9,10 +9,10 @@ import Profile from "@/components/profile";
 export default function Home() {
   return (
     <div>
-      {/* <HeroSection /> */}
-      {/* <LikeButton /> */}
-      {/* <DataTable /> */}
-      {/* <FetchPosts /> */}
+      <HeroSection />
+      <LikeButton />
+      <DataTable />
+      <FetchPosts />
       <Login />
       <Profile />
     </div>
