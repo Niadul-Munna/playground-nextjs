9e8cc0c8cd43856d161458a78d57dba79121e463
niadulMunna <niadulislammunna@gmail.com>
2025-03-15 22:14:41 +0600

    syle : login field

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
     setUser({ username, password });
   };
   return (
-    <div className="borde border-2 p-5 h-96 w-96 m-10">
+    <div className="borde border-2 p-5 h-96 w-96 m-10 ">
       <h1 className="text-2xl mb-8 font-semibold text-center">Login</h1>
       <div className="flex flex-col gap-4">
         <input
@@ -19,17 +19,17 @@ const Login = () => {
           onChange={(e) => setUsername(e.target.value)}
           className="border p-2"
           type="text"
-          placeholder="Enter Username"
+          placeholder="Enter username"
         />
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="border p-2"
           type="password"
-          placeholder="EnterPassword"
+          placeholder="Enter password"
         />
         <button
-          className="border cursor-pointer bg-blue-600 rounded-2xl  px-4 py-1"
+          className="border cursor-pointer bg-blue-400 rounded-2xl px-4 py-1"
           onClick={handleSubmit}
         >
           Login
