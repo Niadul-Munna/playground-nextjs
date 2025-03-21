1ed9bc293393856a91bb1e05cb30391f7b7c3a33
niadulMunna <niadulislammunna@gmail.com>
2025-03-21 21:57:35 +0600

    fix: folder name Conflict

--- src/app/components/filter-items.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { data } from "@/app/data/data";
-const FilterItems = () => {
-  console.log(data);
-  return <div></div>;
-};
-
-export default FilterItems;
