2f43a478db8746afc126b1350ca6d2f171a53dc3
niadulMunna <niadulislammunna@gmail.com>
2025-03-26 02:01:42 +0600

    new page for react hooks tesing

---
+++ src/app/(pages)/components/page.tsx
@@ -0,0 +1,12 @@
+import Reactmemo from "@/hooks/react-memo";
+import React from "react";
+
+const Componentspage = () => {
+  return (
+    <div className="w-full h-screen mx-auto ">
+      <Reactmemo />
+    </div>
+  );
+};
+
+export default Componentspage;
