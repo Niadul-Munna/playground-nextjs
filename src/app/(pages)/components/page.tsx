fab974dcae14cb840761d6fb4dc61eb5e88a6b45
niadulMunna <niadulislammunna@gmail.com>
2025-03-28 03:24:26 +0600

    removed bg-color

--- src/app/(pages)/components/page.tsx
+++ src/app/(pages)/components/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Componentspage = () => {
   return (
-    <div className="w-full h-screen bg-amber-200">
+    <div className="w-full h-screen ">
       <Reactmemo />
     </div>
   );
