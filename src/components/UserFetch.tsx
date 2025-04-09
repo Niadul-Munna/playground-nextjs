e3c3f5c16372ff13a1917efa05c819a3c077e1ec
niadulMunna <niadulislammunna@gmail.com>
2025-04-09 23:52:32 +0600

    api: fetch the user

--- src/components/UserFetch.tsx
+++ src/components/UserFetch.tsx
@@ -1,20 +1,20 @@
-// "use client";
-// import React, { useState } from "react";
+"use client";
+import React, { useState } from "react";
 
-// const Components = () => {
-//   const [users, setUsers] = useState([]);
+const Components = () => {
+  const [users, setUsers] = useState([]);
 
-//   fetch("https://jsonplaceholder.typicode.com/todos/user")
-//     .then((response) => response.json())
-//     .then(setUsers);
+  fetch("https://jsonplaceholder.typicode.com/todos/user")
+    .then((response) => response.json())
+    .then(setUsers);
 
-//   return (
-//     <div className="flex flex-col items-center justify-center">
-//       {users.map((user, inx) => (
-//         <div key={inx}>{user}</div>
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {users.map((user, inx) => (
+        <div key={inx}>{user}</div>
+      ))}
+    </div>
+  );
+};
 
-// export default Components;
+export default Components;
